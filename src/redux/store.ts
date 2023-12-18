f629fb6d3e6f3dbff2f3b7ec556dfb8f13a96904
Lev-FB <146657897+Lev-FB@users.noreply.github.com>
2023-12-19 02:20:19 +0300

    Delete src directory

--- src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import CartSlice from "./cartSlice";
-const store = configureStore({
-    reducer:{
-        cart:CartSlice,
-    }
-})
-
-export default store
